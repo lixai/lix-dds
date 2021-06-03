148269c7dbd3288c0b40bc1fe5eefaea504be488
Michael <311155@qq.com>
2021-06-03 10:29:38 +0800

    Add project directories.

---
+++ examples/examples.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("Not implement examples.");
+}
--- src/dds/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod structure;
-pub mod utils;
-pub mod behavior;
-pub mod common;
-pub mod discovery;
-pub mod messages;
